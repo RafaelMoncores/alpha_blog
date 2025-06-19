21f6b62fb6fdb5de928ed9370ae1ea1d4f13e884
Rafael Moncores <rafamoncores@gmail.com>
2025-06-19 17:28:48 -0300

    feat: new article page and validations messages done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,16 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was successfully created." # Flash message for successful creation
+      redirect_to @article # Redirects to the show action of the article
+    else
+      render :new, status: :unprocessable_entity # Renders the new template again if save fails
+    end
+  end
 end
