80e2eb5d6708575c12609b48928cf8c80254c642
Rafael Moncores <rafamoncores@gmail.com>
2025-06-19 17:46:15 -0300

    feat: add edit and update function, you can access it by /articles/1/edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -15,6 +18,16 @@ class ArticlesController < ApplicationController
       redirect_to @article # Redirects to the show action of the article
     else
       render :new, status: :unprocessable_entity # Renders the new template again if save fails
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was successfully updated." # Flash message for successful update
+      redirect_to @article# Redirects to the show action of the article
+    else
+      render :edit, status: :unprocessable_entity # Renders the edit template again if save fails
     end
   end
 end
