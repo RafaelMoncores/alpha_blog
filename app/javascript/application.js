f2ace3fd661c92bb80d6aa1df210846e8a31e52c
Rafael Moncores <rafamoncores@gmail.com>
2025-06-20 23:17:16 -0300

    feat: redo show page fix: I made the delete button work again

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs"
+Rails.start()
 import "controllers"
 import "bootstrap" // Importa o JavaScript do Bootstrap
 import "@popperjs/core" // Importa o Popper.js
