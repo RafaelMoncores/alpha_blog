0fac2a46542bad6a3836a825a74703cc49405828
Rafael Moncores <rafamoncores@gmail.com>
2025-06-21 12:03:07 -0300

    feat: add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  # regex == regular expression
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
