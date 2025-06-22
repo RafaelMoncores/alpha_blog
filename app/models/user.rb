db707ac305c663c017071c9ac2b9965831dfa20b
Rafael Moncores <rafamoncores@gmail.com>
2025-06-22 08:47:07 -0300

    feat: add bcrypt system for safe password system

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   # regex == regular expression
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  has_secure_password
 end
