e28b3c0d2323d33547222a91a0ed690899e50f8a
Rafael Moncores <rafamoncores@gmail.com>
2025-06-22 08:26:28 -0300

    feat: add alter object state before_save it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   # regex == regular expression
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
