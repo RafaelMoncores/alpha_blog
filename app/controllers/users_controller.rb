a9e13c9c6a244b03cd90edef4b87dd82ccb452e1
Rafael Moncores <rafamoncores@gmail.com>
2025-06-24 20:04:13 -0300

    feat: add authentication of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
   def index
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id # Log the user in after successful signup
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -19,10 +20,8 @@ class UsersController < ApplicationController
     end
   end
   def edit
-    @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account was updated successfully"
       redirect_to @user
@@ -33,5 +32,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.fin(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
     image_src = "https://www.gravatar.com/avatar/#{hash}?#{params}"
     image_tag(image_src, alt: user.username, class: "gravatar rounded shadow mx-auto d-block") # Adicionando uma classe
   end
+
+  # ele vai retornar o usuário atual, se não tiver um, ele faz essa função aí
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # !! converte o valor para booleano
+  def logged_in?
+    !current_user.nil?
+  end
 end
