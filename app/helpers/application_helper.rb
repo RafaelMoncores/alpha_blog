6a253c04bf36de1f58fdd5669ad0b5f06c14027c
Rafael Moncores <rafamoncores@gmail.com>
2025-06-24 08:36:28 -0300

    feat: add users show page with profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 200 })
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+    size = options[:size]
+    default_image = options[:default] || "identicon" # Ou sua URL padrão
+
+    params = URI.encode_www_form("d" => default_image, "s" => size)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?#{params}"
+    image_tag(image_src, alt: user.username, class: "gravatar rounded shadow mx-auto d-block") # Adicionando uma classe
+  end
 end
