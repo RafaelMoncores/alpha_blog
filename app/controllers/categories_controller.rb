2982ec8d83d4dbc10f55ac405b3c2873ae76ce70
Rafael Moncores <rafamoncores@gmail.com>
2025-07-03 16:33:13 -0300

    feat: add edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,6 +12,21 @@ class CategoriesController < ApplicationController
       render "new"
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render "edit"
+    end
+  end
+
   def index
     if params[:search]
       # Se houver busca, pagine os resultados da busca
